9b41dcf640ea2721203c9c0252fcedeb223fd1b3
Pranav <pranav.manik@gmail.com>
2018-11-04 01:36:37 -0400

    grab easyApply apps

--- app_filler.py
+++ app_filler.py
@@ -8,10 +8,11 @@ page_url = input("Enter URL: ")
 
 
 #requests page through User Agent Mozilla
-page_response = requests.get(page_url, timeout=5,  headers={'User-Agent': 'Mozilla/5.0'})
+page_response = requests.get(page_url,  headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(page_response.content, "html.parser")
 
 print(soup.prettify())
+print(soup.findAll('div', attrs={"class":"easyApply"}))
 
-
-
+f = open("webpage.txt", "w")
+f.write(soup.prettify())
