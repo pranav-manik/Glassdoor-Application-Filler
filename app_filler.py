e5b217f8758b0ec631d1083f80a4db821a0ba5db
Pranav <pranav.manik@gmail.com>
2018-11-05 23:18:22 -0500

    grab easyApps

--- app_filler.py
+++ app_filler.py
@@ -11,8 +11,19 @@ page_url = input("Enter URL: ")
 page_response = requests.get(page_url,  headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(page_response.content, "html.parser")
 
-print(soup.prettify())
-print(soup.findAll('div', attrs={"class":"easyApply"}))
+#print(soup.prettify())
+
+# All Jobs
+# soup.find_all("li", attrs={"class":"jl"})
+
+i = 1
+for job in soup.find_all("li", attrs={"data-is-easy-apply":"true"}):
+
+	print("Job Number:", i)
+	print(job.prettify())
+	
+
+	i=i+1
 
 f = open("webpage.txt", "w")
 f.write(soup.prettify())
